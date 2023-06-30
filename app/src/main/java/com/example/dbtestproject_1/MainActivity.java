79fd0427fc41184f8915c589370fdb7e42c75cc4
woodawon <woodawon0602@gmail.com>
2023-07-01 01:58:38 +0900

    wow

---
+++ app/src/main/java/com/example/dbtestproject_1/MainActivity.java
@@ -0,0 +1,57 @@
+package com.example.dbtestproject_1;
+
+import static java.sql.DriverManager.println;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText editText1;
+    EditText editText2;
+    Button button1;
+    Button button2;
+    TextView textView;
+
+    SQLiteDatabase database;
+    String tableName;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        editText1 = findViewById(R.id.editText1);
+        editText2 = findViewById(R.id.editText2);
+        textView = findViewById(R.id.textView);
+
+        button1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                String databaseName = editText1.getText().toString();
+                createDatabase(databaseName);
+            }
+        });
+
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+    }
+
+    private void createDatabase(String name) {
+        println("createDatabase 호출됨!");
+        database = openOrCreateDatabase(name, MODE_PRIVATE, null);
+        println("DB 생성함 : " + name);
+    }
+
+}
